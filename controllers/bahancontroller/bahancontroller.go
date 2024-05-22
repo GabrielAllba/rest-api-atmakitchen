b639e67a44503be38f4f3c3b61a847ae3e00438b
zyrelll <jayadewatabagaskara@gmail.com>
2024-05-22 11:30:19 +0700

    bahan

--- controllers/bahancontroller/bahancontroller.go
+++ controllers/bahancontroller/bahancontroller.go
@@ -116,7 +116,7 @@ func Update(c *gin.Context) {
 		return
 	}
 
-	if bahan.Nama == "" || bahan.Merk == "" || bahan.Satuan == "" {
+	if bahan.Nama == "" || bahan.Satuan == "" {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Pastikan semua input terisi"})
 		return
 	}
